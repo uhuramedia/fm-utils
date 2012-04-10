bb02ad00339f509581bda3c5097be825ac6f5f19
Julian Bez <julian@freshmilk.tv>
2012-04-10 11:06:09 +0200

    Fix some bugs with evaluation to bool

--- fm/utils/image.py
+++ fm/utils/image.py
@@ -18,18 +18,22 @@ def scale_and_crop(im, width, height, upscale, crop, center_y=True, sharpen=True
     else:
         r = min(xr / x, yr / y)
 
-    if r < 1.0 or (r > 1.0 and upscale):
+    modified = False
+    if r < 1 or (r > 1 and upscale == True):
         im = im.resize((int(x * r), int(y * r)), resample=Image.ANTIALIAS)
+        modified = True
 
     if crop:
         x, y = [Decimal(v) for v in im.size]
         ex, ey = (x - min(x, xr)) / 2, (y - min(y, yr)) / 2
-        if center_y and (ex or ey):
+        if center_y == True and (ex > 0 or ey > 0):
             im = im.crop((int(ex), int(ey), int(x - ex), int(y - ey)))
-        elif not center_y and (ex or ey):
+            modified = True
+        elif not center_y == True and (ex > 0 or ey > 0):
             im = im.crop((int(ex), 0.0, int(x - ex), int(y - ey - ey)))
+            modified = True
 
-    if sharpen:
+    if sharpen and modified:
         fltr = ImageFilter.Kernel((3, 3),
                   (-1, -1, -1,
                    - 1, 22, -1,
