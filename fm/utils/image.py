9043ee8dd9498bcdd5beba2c15913f5d150b7d9d
Freshmilk TV01 <developers@freshmilk.tv>
2012-10-31 12:37:52 +0100

    Fix error in crop

--- fm/utils/image.py
+++ fm/utils/image.py
@@ -34,7 +34,7 @@ def scale_and_crop(im, width, height, upscale, crop, center_y=True, sharpen=True
             im = im.crop((int(ex), int(ey), int(x - ex), int(y - ey)))
             modified = True
         elif not center_y == True and (ex > 0 or ey > 0):
-            im = im.crop((int(ex), 0.0, int(x - ex), int(y - ey - ey)))
+            im = im.crop((int(ex), 0, int(x - ex), int(y - ey - ey)))
             modified = True
 
     if sharpen and modified:
